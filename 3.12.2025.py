dd84c64daebea6a692229d2d0fa8ae2570751c6d
asm1127 <marjona1marjona2@gmail.com>
2025-12-03 17:48:30 +0500

    mashq

---
+++ 3.12.2025.py
@@ -0,0 +1,23 @@
+# def celsius_to_fahr(temp):
+#     return 9/5 * temp + 32
+# print(celsius_to_fahr(1))
+
+
+# def square(number):
+#     return number * number
+#
+# def cube(number):
+#     return number * square(number)
+#
+# print("Square of 2 is", square(2))
+# print("Cube of 3  is", cube(2))
+
+
+# def salomlashish(ism, fam, ot):
+#     return f"Assalomu aleykum {ism} {fam} {ot}, jamoamizga xush kelibsiz!"
+#
+# ism = input("Ismingizni keiring: ")
+# fam = input("Familiyangizni keiring: ")
+# ot = input("Sharifingizni keiring: ")
+#
+# print(salomlashish(ism, fam, ot))
