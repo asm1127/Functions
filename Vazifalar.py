8f0ec268b43af7f2e42f90a140ba30e491612c59
asm1127 <marjona1marjona2@gmail.com>
2025-12-05 17:48:53 +0500

    yechim

---
+++ Vazifalar.py
@@ -0,0 +1,34 @@
+# #1
+# def katta_son(a, b):
+#     if a > b:
+#         return f"A son katta"
+#     else:
+#         return f"B son katta"
+#
+# son1 = int(input("A: "))
+# son2 = int(input("B: "))
+#
+# print(katta_son(son1, son2))
+
+# #2
+# def teskari(matn):
+#     return matn[::-1]
+#
+# a = input("Matn kiriting: ")
+# print(teskari(a))
+
+# #3
+# def katta_harf_bilan_boshlash(matn):
+#     return matn
+#
+# a = input("Mayn kiriting: ").title()
+# print(katta_harf_bilan_boshlash(a))
+
+# #4
+# def palidrom(soz):
+#     x = soz[::-1]
+#     return f"{soz} ni teskarisi: {x}"
+#
+# x = input("So'zni kiriting: ")
+#
+# print(palidrom(x))
